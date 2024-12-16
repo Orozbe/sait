a08c5666ac42fd4c85c0cee29f0b7f9d9bb34fc4
Orozbe <126249529+Orozbe@users.noreply.github.com>
2024-12-16 23:04:30 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => {
+        section.classList.remove('active');
+    });
+
+    const targetSection = document.getElementById(sectionId);
+    if (targetSection) {
+        targetSection.classList.add('active');
+    }
+}
